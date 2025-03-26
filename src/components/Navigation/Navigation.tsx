cbc38e9af328b08da12763a73d044182d9a7d105
Jason Lang <raptorify@gmail.com>
2025-03-26 19:50:05 +0100

    Add redirect and navigation highlighting

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,27 +1,55 @@
+"use client";
+
+import {useState, useEffect} from "react";
+import {useSelectedLayoutSegment} from "next/navigation";
 import {Me} from "@/app/types";
 import {API_URL} from "@/app/constants";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./Navigation.module.css"
+import styles from "./Navigation.module.css";
 
-export default async function Navigation() {
-    const response = await fetch(`${API_URL}/users/me`);
-    const data: Me = await response.json();
+export default function Navigation() {
+    const [data, setData] = useState<Me | null>(null);
+    const selectedSegment = useSelectedLayoutSegment();
 
-    return <nav className={styles.navigation}>
-        <ul className={styles.list}>
-            <li>
-                <Link href={"/rooms"} className={styles.listItem}>Rooms</Link>
-            </li>
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`${API_URL}/users/me`);
+            const result: Me = await response.json();
+            setData(result);
+        };
 
-            <li>
-                <Link href={"/create"} className={styles.listItem}>Create</Link>
-            </li>
-        </ul>
+        fetchData();
+    }, []);
 
-        <div className={styles.profile}>
-            <span>{data.firstName + ' ' + data.lastName}</span>
-            <Image src={data.portraitUrl} alt="Profile picture" width={40} height={40} />
-        </div>
-    </nav>;
+    return (
+        <nav className={styles.navigation}>
+            <ul className={styles.list}>
+                <li>
+                    <Link
+                        href="/rooms"
+                        className={`${styles.listItem} ${selectedSegment === 'rooms' ? styles.active : ''}`}
+                    >
+                        Rooms
+                    </Link>
+                </li>
+
+                <li>
+                    <Link
+                        href="/create"
+                        className={`${styles.listItem} ${selectedSegment === 'create' ? styles.active : ''}`}
+                    >
+                        Create
+                    </Link>
+                </li>
+            </ul>
+
+            {data && (
+                <div className={styles.profile}>
+                    <span>{data.firstName + ' ' + data.lastName}</span>
+                    <Image src={data.portraitUrl} alt="Profile picture" width={40} height={40}/>
+                </div>
+            )}
+        </nav>
+    );
 }
